abf0dcca8da885b63fd046ef817b62d71a231944
ejonghwan <sun87-1@daum.net>
2023-06-26 12:52:47 +0900

    init project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import { useState } from 'react';
-import './scss/style.scss';
-import Layout from './components/common/Layout.js'
-
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
+import Visual from './components/main/Visual';
+import Community from './components/sub/Community';
+import Contact from './components/sub/Contact';
+import Department from './components/sub/Department';
+import Gallery from './components/sub/Gallery';
+import Member from './components/sub/Member';
+import Youtube from './components/sub/Youtube';
 
 function App() {
+	return (
+		<>
+			<Header />
 
-  return (
-    <div className="App">
-      <Layout />
-    </div>
-  );
+			<Visual />
+			<Department />
+			<Community />
+			<Gallery />
+			<Youtube />
+			<Contact />
+			<Member />
+
+			<Footer />
+		</>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-// 18v
-// import React from 'react';
-// import ReactDOM from 'react-dom/client'; //18버전 리액트돔 
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
